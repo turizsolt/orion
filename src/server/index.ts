524a7bf5b4033654a95a769846fd60f928358712
turizsolt <turi.zsolt.m@gmail.com>
2020-01-14 15:53:03 +0100

    feat: passkey barrier added front of admin api's

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import * as fastify from 'fastify';
+import * as fs from 'fs';
 import { AddressInfo } from 'net';
 import { runContainer } from '../inversify.config';
 import { Business } from '../logic/Business';
@@ -11,6 +12,20 @@ const app = fastify({ logger: true });
 app.get('/', async () => {
     return { hello: 'world' };
 });
+
+interface Config {
+    adminKey: string;
+}
+
+const configText = fs.readFileSync(process.cwd() + '/config.json', 'utf8');
+const config: Config = JSON.parse(configText);
+
+const adminBarrier = (request: fastify.FastifyRequest) => {
+    const { key } = request.query;
+    if (key !== config.adminKey) {
+        throw new Error('No permissions for the operation.');
+    }
+};
 
 const postSchema = {
     body: {
@@ -73,6 +88,10 @@ app.get('/election/:id', { schema: idSchema }, async request => {
 
 app.get('/election/:id/result', { schema: idSchema }, async request => {
     const { id } = request.params;
+    const election = business.getElection(id);
+    if (election.open) {
+        adminBarrier(request);
+    }
     const electionResult = business.getElectionResult(id);
     return electionResult;
 });
@@ -85,12 +104,16 @@ app.post('/election/:id/vote', { schema: postSchema }, async request => {
 });
 
 app.post('/election/:id/close', { schema: idSchema }, async request => {
+    adminBarrier(request);
+
     const id = request.params.id;
     business.closeElection(id);
     return true;
 });
 
 app.post('/election', { schema: postElectionSchema }, async request => {
+    adminBarrier(request);
+
     const { options, name } = request.body;
     business.createElection(name, options);
     return true;
