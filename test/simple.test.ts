acd5faf0a7c2a8cd7b9ca446dab92a48f51d24a7
turizsolt <turi.zsolt.m@gmail.com>
2020-01-15 00:58:27 +0100

    fiddling: simple http server to travis

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import * as fetch from 'node-fetch';
 
+/*
 import * as http from 'http';
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/json' });
@@ -9,9 +10,30 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8080);
+*/
+
+// tslint:disable-next-line: no-var-requires
+const fastify = require('fastify')({ logger: true });
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' };
+});
+
+const start = async () => {
+    try {
+        await fastify.listen(8080);
+        fastify.log.info(
+            `server listening on ${fastify.server.address().port}`,
+        );
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+start();
 
 setTimeout(() => {
-    server.close();
+    // server.close();
+    fastify.close();
 }, 60 * 1000);
 
 describe('Localhost', () => {
