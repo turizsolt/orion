8f3dd11b639203059562ca83f001dbb8342b958c
turizsolt <turi.zsolt.m@gmail.com>
2020-01-14 22:16:48 +0100

    fiddling: fiddling with travis

--- Config.ts
+++ Config.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs';
 export let config = {
     adminKey: '',
     testServerAddress: 'http://127.0.0.1',
-    testServerPort: 3000,
-    port: 3000,
+    testServerPort: 9865,
+    port: 9865,
 };
 if (fs.existsSync(process.cwd() + '/config.json')) {
     const configText = fs.readFileSync(process.cwd() + '/config.json', 'utf8');
